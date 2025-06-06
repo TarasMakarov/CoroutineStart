52791b94e3d7c52a1b325ed1ed7074d77ff7fb93
Taras <tarmakar@gmail.com>
2025-06-06 17:23:01 +0300

    8.5 - Coroutines "under the hood"

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.sumin.coroutinestart
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 loadData()
             }
+//            loadWithoutCoroutine()
         }
     }
 
@@ -39,9 +42,44 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity1", "loadData() finished: $this")
     }
 
+    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                Log.d("MainActivity1", "loadData() start: $this")
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(1, it)
+                }
+            }
+
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                loadTemperatureWithoutCoroutines(city) {
+                    loadWithoutCoroutine(2, it)
+                }
+            }
+
+            2 -> {
+                val temp = obj as Int
+                binding.tvTemperature.text = "$temp"
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+                Log.d("MainActivity1", "loadData() finished: $this")
+            }
+        }
+    }
+
     private suspend fun loadCity(): String {
         delay(5000)
         return "Moscow"
+    }
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback("Moscow")
+        }, 5000)
     }
 
     private suspend fun loadTemperature(city: String): Int {
@@ -52,5 +90,16 @@ class MainActivity : AppCompatActivity() {
         ).show()
         delay(5000)
         return 17
+    }
+
+    private fun loadTemperatureWithoutCoroutines(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback(17)
+        }, 5000)
     }
 }
